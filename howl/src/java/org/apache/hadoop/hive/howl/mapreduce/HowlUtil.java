862c5860f6993c66316c57b8705da09f949f8eb2
khorgath <khorgath@gmail.com>
2010-07-09 17:29:10 -0700

    Delinking from ObjectSerializer

--- howl/src/java/org/apache/hadoop/hive/howl/mapreduce/HowlUtil.java
+++ howl/src/java/org/apache/hadoop/hive/howl/mapreduce/HowlUtil.java
@@ -18,11 +18,14 @@
 
 package org.apache.hadoop.hive.howl.mapreduce;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 import org.apache.hadoop.mapreduce.JobContext;
-import org.apache.pig.impl.util.ObjectSerializer;
 
 public class HowlUtil {
 
@@ -33,12 +36,53 @@ public class HowlUtil {
     return true;
   }
 
-  public static Object deserialize(String representation) throws IOException{
-    return ObjectSerializer.deserialize(representation);
+  public static String serialize(Serializable obj) throws IOException {
+    if (obj == null) {
+      return "";
+    }
+    try {
+      ByteArrayOutputStream serialObj = new ByteArrayOutputStream();
+      ObjectOutputStream objStream = new ObjectOutputStream(serialObj);
+      objStream.writeObject(obj);
+      objStream.close();
+      return encodeBytes(serialObj.toByteArray());
+    } catch (Exception e) {
+      throw new IOException("Serialization error: " + e.getMessage(), e);
+    }
   }
 
-  public static String serialize(Serializable o) throws IOException{
-    return ObjectSerializer.serialize(o);
+  public static Object deserialize(String str) throws IOException {
+    if (str == null || str.length() == 0) {
+      return null;
+    }
+    try {
+      ByteArrayInputStream serialObj = new ByteArrayInputStream(decodeBytes(str));
+      ObjectInputStream objStream = new ObjectInputStream(serialObj);
+      return objStream.readObject();
+    } catch (Exception e) {
+      throw new IOException("Deserialization error: " + e.getMessage(), e);
+    }
   }
 
+  public static String encodeBytes(byte[] bytes) {
+    StringBuffer strBuf = new StringBuffer();
+
+    for (int i = 0; i < bytes.length; i++) {
+      strBuf.append((char) (((bytes[i] >> 4) & 0xF) + ('a')));
+      strBuf.append((char) (((bytes[i]) & 0xF) + ('a')));
+    }
+
+    return strBuf.toString();
+  }
+
+  public static byte[] decodeBytes(String str) {
+    byte[] bytes = new byte[str.length() / 2];
+    for (int i = 0; i < str.length(); i+=2) {
+      char c = str.charAt(i);
+      bytes[i/2] = (byte) ((c - 'a') << 4);
+      c = str.charAt(i+1);
+      bytes[i/2] += (c - 'a');
+    }
+    return bytes;
+  }
 }
