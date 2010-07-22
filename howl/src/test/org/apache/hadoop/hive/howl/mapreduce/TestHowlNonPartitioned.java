adeb282fd8b6039d3aa9e45a7446074bb0bb9cf7
Ajay Kidave <kidave@driedfrom.corp.yahoo.com>
2010-07-22 03:35:38 -0700

    Added hadoop local mode test for partitioned and non partitioned tables

---
+++ howl/src/test/org/apache/hadoop/hive/howl/mapreduce/TestHowlNonPartitioned.java
@@ -0,0 +1,109 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.hive.howl.mapreduce;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.hadoop.hive.howl.data.DefaultHowlRecord;
+import org.apache.hadoop.hive.howl.data.HowlFieldSchema;
+import org.apache.hadoop.hive.howl.data.HowlRecord;
+import org.apache.hadoop.hive.metastore.api.FieldSchema;
+import org.apache.hadoop.hive.serde.Constants;
+
+public class TestHowlNonPartitioned extends HowlMapReduceTest {
+
+  @Override
+  protected void initialize() {
+
+    writeRecords = new ArrayList<HowlRecord>();
+
+    for(int i = 0;i < 20;i++) {
+      List<Object> objList = new ArrayList<Object>();
+
+      objList.add(i);
+      objList.add("strvalue" + i);
+      writeRecords.add(new DefaultHowlRecord(objList));
+    }
+  }
+
+
+  @Override
+  protected List<HowlFieldSchema> getPartitionColumns() {
+    List<HowlFieldSchema> fields = new ArrayList<HowlFieldSchema>();
+    fields.add(new HowlFieldSchema("c1", Constants.INT_TYPE_NAME, ""));
+    fields.add(new HowlFieldSchema("c2", Constants.STRING_TYPE_NAME, ""));
+    return fields;
+  }
+
+  @Override
+  protected List<FieldSchema> getPartitionKeys() {
+    List<FieldSchema> fields = new ArrayList<FieldSchema>();
+    //empty list, non partitioned
+    return fields;
+  }
+
+  @Override
+  protected List<FieldSchema> getTableColumns() {
+    List<FieldSchema> fields = new ArrayList<FieldSchema>();
+    fields.add(new FieldSchema("c1", Constants.INT_TYPE_NAME, ""));
+    fields.add(new FieldSchema("c2", Constants.STRING_TYPE_NAME, ""));
+    return fields;
+  }
+
+
+  public void testHowlNonPartitionedTable() throws Exception {
+
+    Map<String, String> partitionMap = new HashMap<String, String>();
+    runMRCreate(null, 10);
+
+    //Test for duplicate publish
+    IOException exc = null;
+    try {
+      runMRCreate(null, 20);
+    } catch(IOException e) {
+      exc = e;
+    }
+
+    assertTrue(exc != null);
+    assertTrue(exc.getMessage().indexOf(
+        "already contains data") != -1);
+
+    //Test for publish with invalid partition key name
+    exc = null;
+    partitionMap.clear();
+    partitionMap.put("px", "p1value2");
+
+    try {
+      runMRCreate(partitionMap, 20);
+    } catch(IOException e) {
+      exc = e;
+    }
+
+    assertTrue(exc != null);
+    assertTrue(exc.getMessage().indexOf(
+        "Invalid partition values specified") != -1);
+
+    //Read should get 10 rows
+    runMRRead(10);
+  }
+}
