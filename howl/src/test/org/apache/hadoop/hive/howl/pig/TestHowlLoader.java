9bd2b09ee703f40f3cf58f9e45de35121c0e087f
khorgath <khorgath@gmail.com>
2010-08-05 11:21:03 -0700

    misc test fixes, guarded startup and shutdown, undisabling a couple of tests

--- howl/src/test/org/apache/hadoop/hive/howl/pig/TestHowlLoader.java
+++ howl/src/test/org/apache/hadoop/hive/howl/pig/TestHowlLoader.java
@@ -27,14 +27,16 @@ public class TestHowlLoader extends TestCase {
   private static final String BASIC_TABLE = "junit_unparted_basic";
   private static final String COMPLEX_TABLE = "junit_unparted_complex";
   private static final String PARTITIONED_TABLE = "junit_parted_basic";
-  MiniCluster cluster = MiniCluster.buildCluster();
-  private Driver driver;
+  private static MiniCluster cluster = MiniCluster.buildCluster();
+  private static Driver driver;
 
-  String basicFile = "basic.input.data";
-  String complexFile = "complex.input.data";
+  private static final String basicFile = "basic.input.data";
+  private static final String complexFile = "complex.input.data";
 
-  Map<Integer,Pair<Integer,String>> basicInputData;
-  Map<Integer,Tuple> complexInputData;
+  private static int guardTestCount = 4; // ugh, instantiate using introspection in guardedSetupBeforeClass
+  private static boolean setupHasRun = false;
+
+  private static Map<Integer,Pair<Integer,String>> basicInputData;
 
   private void dropTable(String tablename) throws IOException{
     driver.run("drop table "+tablename);
@@ -49,7 +51,7 @@ public class TestHowlLoader extends TestCase {
         "'howl.osd'='org.apache.hadoop.hive.howl.drivers.RCFileOutputStorageDriver') ";
     int retCode = driver.run(createTable).getResponseCode();
     if(retCode != 0) {
-      throw new IOException("Failed to create table. ["+createTable+"]");
+      throw new IOException("Failed to create table. ["+createTable+"], return code from hive driver : ["+retCode+"]");
     }
   }
 
@@ -57,10 +59,14 @@ public class TestHowlLoader extends TestCase {
     createTable(tablename,schema,null);
   }
 
-  @Override
-  protected void setUp() throws Exception {
-
-    // TODO: this really really needs to move to a setupBeforeClass, but that's not working
+  protected void guardedSetUpBeforeClass() throws Exception {
+    if (!setupHasRun){
+      setupHasRun = true;
+      System.out.println("running setUp for the first time.");
+    }else{
+      System.out.println("setUp has already run previously, skipping.");
+      return;
+    }
 
     HiveConf hiveConf = new HiveConf(this.getClass());
     hiveConf.set(HiveConf.ConfVars.PREEXECHOOKS.varname, "");
@@ -70,14 +76,14 @@ public class TestHowlLoader extends TestCase {
     cleanup();
 
     createTable(BASIC_TABLE,"a int, b string");
-//    createTable(COMPLEX_TABLE,
-//        "name string, studentid int, "
-//        + "contact struct<phno:string,email:string>, "
-//        + "currently_registered_courses array<string>, "
-//        + "current_grades map<string,string>, "
-//        + "phnos array<struct<phno:string,type:string>");
+    createTable(COMPLEX_TABLE,
+        "name string, studentid int, "
+        + "contact struct<phno:string,email:string>, "
+        + "currently_registered_courses array<string>, "
+        + "current_grades map<string,string>, "
+        + "phnos array<struct<phno:string,type:string>>");
 
-//    createTable(PARTITIONED_TABLE,"a int, b string","bkt int");
+    createTable(PARTITIONED_TABLE,"a int, b string","bkt int");
 
 
     int LOOP_SIZE = 3;
@@ -95,12 +101,12 @@ public class TestHowlLoader extends TestCase {
     }
     MiniCluster.createInputFile(cluster, basicFile, input);
 
-//    MiniCluster.createInputFile(cluster, complexFile,
-//        new String[]{
-//        "Henry Jekyll\t42\t(415-253-6367,hjekyll@contemporary.edu.uk)\t{(PHARMACOLOGY),(PSYCHIATRY)},[PHARMACOLOGY#A-,PSYCHIATRY#B+],{(415-253-6367,cell),(408-253-6367,landline)}",
-//        "Edward Hyde\t1337\t(415-253-6367,anonymous@b44chan.org)\t{(CREATIVE_WRITING),(COPYRIGHT_LAW)},[CREATIVE_WRITING#A+,COPYRIGHT_LAW#D],{(415-253-6367,cell),(408-253-6367,landline)}",
-//        }
-//    );
+    MiniCluster.createInputFile(cluster, complexFile,
+        new String[]{
+        "Henry Jekyll\t42\t(415-253-6367,hjekyll@contemporary.edu.uk)\t{(PHARMACOLOGY),(PSYCHIATRY)},[PHARMACOLOGY#A-,PSYCHIATRY#B+],{(415-253-6367,cell),(408-253-6367,landline)}",
+        "Edward Hyde\t1337\t(415-253-6367,anonymous@b44chan.org)\t{(CREATIVE_WRITING),(COPYRIGHT_LAW)},[CREATIVE_WRITING#A+,COPYRIGHT_LAW#D],{(415-253-6367,cell),(408-253-6367,landline)}",
+        }
+    );
 
     PigServer server = new PigServer(ExecType.MAPREDUCE, cluster.getProperties());
     UDFContext.getUDFContext().setClientSystemProps();
@@ -111,22 +117,37 @@ public class TestHowlLoader extends TestCase {
 //    server.registerQuery("store B into '"+PARTITIONED_TABLE+"' using org.apache.hadoop.hive.howl.pig.HowlStorer('bkt=0');");
 //    server.registerQuery("C = filter A by a >= 2;");
 //    server.registerQuery("store C into '"+PARTITIONED_TABLE+"' using org.apache.hadoop.hive.howl.pig.HowlStorer('bkt=1');");
-//    server.registerQuery("D = load '"+complexFile+"' as (name:string, studentid:int, contact:tuple(phno:string,email:string), currently_registered_courses:bag{innertup:tuple(course:string)}, current_grades:map[ ] , phnos :bag{innertup:tuple(phno:string,type:string)})");
-//    server.registerQuery("store D into '"+COMPLEX_TABLE+"' using org.apache.hadoop.hive.howl.pig.HowlStorer();");
+    server.registerQuery("D = load '"+complexFile+"' as (name:chararray, studentid:int, contact:tuple(phno:chararray,email:chararray), currently_registered_courses:bag{innertup:tuple(course:chararray)}, current_grades:map[ ] , phnos :bag{innertup:tuple(phno:chararray,type:chararray)});");
+    server.registerQuery("store D into '"+COMPLEX_TABLE+"' using org.apache.hadoop.hive.howl.pig.HowlStorer();");
     server.executeBatch();
 
   }
   private void cleanup() throws IOException {
     MiniCluster.deleteFile(cluster, basicFile);
-//    MiniCluster.deleteFile(cluster, complexFile);
+    MiniCluster.deleteFile(cluster, complexFile);
     dropTable(BASIC_TABLE);
-//    dropTable(COMPLEX_TABLE);
-//    dropTable(PARTITIONED_TABLE);
+    dropTable(COMPLEX_TABLE);
+    dropTable(PARTITIONED_TABLE);
+  }
+
+  protected void guardedTearDownAfterClass() throws Exception {
+    guardTestCount--;
+    if (guardTestCount > 0){
+      System.out.println("skipping tearDown, "+guardTestCount+" more tests still need to run.");
+      return;
+    }
+    System.out.println("running tearDown.");
+    cleanup();
+  }
+
+  @Override
+  protected void setUp() throws Exception {
+    guardedSetUpBeforeClass();
   }
 
   @Override
   protected void tearDown() throws Exception {
-    cleanup();
+    guardedTearDownAfterClass();
   }
 
   public void testSchemaLoadBasic() throws IOException{
@@ -154,6 +175,7 @@ public class TestHowlLoader extends TestCase {
     int numTuplesRead = 0;
     while( XIter.hasNext() ){
       Tuple t = XIter.next();
+      assertEquals(2,t.size());
       assertTrue(t.get(0).getClass() == Integer.class);
       assertTrue(t.get(1).getClass() == String.class);
       assertEquals(t.get(0),basicInputData.get(numTuplesRead).first);
@@ -163,7 +185,7 @@ public class TestHowlLoader extends TestCase {
     assertEquals(basicInputData.size(),numTuplesRead);
   }
 
-  public void disabled_testSchemaLoadComplex() throws IOException{
+  public void testSchemaLoadComplex() throws IOException{
 
     PigServer server = new PigServer(ExecType.MAPREDUCE, cluster.getProperties());
 
@@ -174,14 +196,14 @@ public class TestHowlLoader extends TestCase {
     List<FieldSchema> Kfields = dumpedKSchema.getFields();
     assertEquals(6,Kfields.size());
 
-    assertTrue(Kfields.get(0).type == DataType.CHARARRAY);
-    assertTrue(Kfields.get(0).alias.equalsIgnoreCase("name"));
+    assertEquals(DataType.CHARARRAY,Kfields.get(0).type);
+    assertEquals("name",Kfields.get(0).alias.toLowerCase());
 
-    assertTrue(Kfields.get(1).type == DataType.INTEGER);
-    assertTrue(Kfields.get(1).alias.equalsIgnoreCase("studentid"));
+    assertEquals( DataType.INTEGER,Kfields.get(1).type);
+    assertEquals("studentid",Kfields.get(1).alias.toLowerCase());
 
-    assertTrue(Kfields.get(2).type == DataType.TUPLE);
-    assertTrue(Kfields.get(2).alias.equalsIgnoreCase("contact"));
+    assertEquals(DataType.TUPLE,Kfields.get(2).type);
+    assertEquals("contact",Kfields.get(2).alias.toLowerCase());
     {
       assertNotNull(Kfields.get(2).schema);
       assertTrue(Kfields.get(2).schema.getFields().size() == 2);
@@ -190,31 +212,33 @@ public class TestHowlLoader extends TestCase {
       assertTrue(Kfields.get(2).schema.getFields().get(1).type == DataType.CHARARRAY);
       assertTrue(Kfields.get(2).schema.getFields().get(1).alias.equalsIgnoreCase("email"));
     }
-    assertTrue(Kfields.get(3).type == DataType.BAG);
-    assertTrue(Kfields.get(3).alias.equalsIgnoreCase("currently_registered_courses"));
+    assertEquals(DataType.BAG,Kfields.get(3).type);
+    assertEquals("currently_registered_courses",Kfields.get(3).alias.toLowerCase());
     {
       assertNotNull(Kfields.get(3).schema);
-      assertTrue(Kfields.get(3).schema.getFields().size() == 1);
-      assertTrue(Kfields.get(3).schema.getFields().get(0).type == DataType.TUPLE);
+      assertEquals(1,Kfields.get(3).schema.getFields().size());
+      assertEquals(DataType.TUPLE,Kfields.get(3).schema.getFields().get(0).type);
       assertNotNull(Kfields.get(3).schema.getFields().get(0).schema);
-      assertTrue(Kfields.get(3).schema.getFields().get(0).schema.getFields().size() == 1);
-      assertTrue(Kfields.get(3).schema.getFields().get(0).schema.getFields().get(0).type == DataType.CHARARRAY);
-      assertTrue(Kfields.get(3).schema.getFields().get(0).schema.getFields().get(0).alias.equalsIgnoreCase("course"));
+      assertEquals(1,Kfields.get(3).schema.getFields().get(0).schema.getFields().size());
+      assertEquals(DataType.CHARARRAY,Kfields.get(3).schema.getFields().get(0).schema.getFields().get(0).type);
+      // assertEquals("course",Kfields.get(3).schema.getFields().get(0).schema.getFields().get(0).alias.toLowerCase());
+      // commented out, because the name becomes "innerfield" by default - we call it "course" in pig,
+      // but in the metadata, it'd be anonymous, so this would be autogenerated, which is fine
     }
-    assertTrue(Kfields.get(4).type == DataType.MAP);
-    assertTrue(Kfields.get(4).alias.equalsIgnoreCase("current_grades"));
-    assertTrue(Kfields.get(5).type == DataType.BAG);
-    assertTrue(Kfields.get(5).alias.equalsIgnoreCase("phnos"));
+    assertEquals(DataType.MAP,Kfields.get(4).type);
+    assertEquals("current_grades",Kfields.get(4).alias.toLowerCase());
+    assertEquals(DataType.BAG,Kfields.get(5).type);
+    assertEquals("phnos",Kfields.get(5).alias.toLowerCase());
     {
       assertNotNull(Kfields.get(5).schema);
-      assertTrue(Kfields.get(5).schema.getFields().size() == 1);
-      assertTrue(Kfields.get(5).schema.getFields().get(0).type == DataType.TUPLE);
+      assertEquals(1,Kfields.get(5).schema.getFields().size());
+      assertEquals(DataType.TUPLE,Kfields.get(5).schema.getFields().get(0).type);
       assertNotNull(Kfields.get(5).schema.getFields().get(0).schema);
       assertTrue(Kfields.get(5).schema.getFields().get(0).schema.getFields().size() == 2);
-      assertTrue(Kfields.get(5).schema.getFields().get(0).schema.getFields().get(0).type == DataType.CHARARRAY);
-      assertTrue(Kfields.get(5).schema.getFields().get(0).schema.getFields().get(0).alias.equalsIgnoreCase("phno"));
-      assertTrue(Kfields.get(5).schema.getFields().get(0).schema.getFields().get(1).type == DataType.CHARARRAY);
-      assertTrue(Kfields.get(5).schema.getFields().get(0).schema.getFields().get(0).alias.equalsIgnoreCase("type"));
+      assertEquals(DataType.CHARARRAY,Kfields.get(5).schema.getFields().get(0).schema.getFields().get(0).type);
+      assertEquals("phno",Kfields.get(5).schema.getFields().get(0).schema.getFields().get(0).alias.toLowerCase());
+      assertEquals(DataType.CHARARRAY,Kfields.get(5).schema.getFields().get(0).schema.getFields().get(1).type);
+      assertEquals("type",Kfields.get(5).schema.getFields().get(0).schema.getFields().get(1).alias.toLowerCase());
     }
 
   }
@@ -227,29 +251,41 @@ public class TestHowlLoader extends TestCase {
     driver.getResults(valuesReadFromHiveDriver);
     assertEquals(basicInputData.size(),valuesReadFromHiveDriver.size());
 
-    server.registerQuery("W = load '"+PARTITIONED_TABLE+"' using org.apache.hadoop.hive.howl.pig.HowlLoader() as (a,b,bkt);");
+    server.registerQuery("W = load '"+PARTITIONED_TABLE+"' using org.apache.hadoop.hive.howl.pig.HowlLoader();");
+    Schema dumpedWSchema = server.dumpSchema("W");
+    List<FieldSchema> Wfields = dumpedWSchema.getFields();
+    assertEquals(3,Wfields.size());
+    assertTrue(Wfields.get(0).alias.equalsIgnoreCase("a"));
+    assertTrue(Wfields.get(0).type == DataType.INTEGER);
+    assertTrue(Wfields.get(1).alias.equalsIgnoreCase("b"));
+    assertTrue(Wfields.get(1).type == DataType.CHARARRAY);
+    assertTrue(Wfields.get(2).alias.equalsIgnoreCase("bkt"));
+    assertTrue(Wfields.get(2).type == DataType.INTEGER);
+
     Iterator<Tuple> WIter = server.openIterator("W");
     Collection<Pair<Integer,String>> valuesRead = new ArrayList<Pair<Integer,String>>();
     while( WIter.hasNext() ){
       Tuple t = WIter.next();
+      assertTrue(t.size() == 3);
       assertTrue(t.get(0).getClass() == Integer.class);
       assertTrue(t.get(1).getClass() == String.class);
+      assertTrue(t.get(2).getClass() == Integer.class);
       valuesRead.add(new Pair<Integer,String>((Integer)t.get(0),(String)t.get(1)));
     }
     assertEquals(valuesReadFromHiveDriver.size(),valuesRead.size());
     assertEquals(valuesRead,basicInputData.values());
   }
 
-  public void disabled_testProjectionsBasic() throws IOException {
+  public void testProjectionsBasic() throws IOException {
 
     PigServer server = new PigServer(ExecType.MAPREDUCE, cluster.getProperties());
 
-    // test that projections work - note, yes, this test currently fails.
-    // But it does show what we hope we understand to be desired behaviour.
-    // so while the test is disabled for now, we expect to resurrect it sometime
+    // projections are handled by using generate
+    // TODO : attempt to reorder columns
 
-    server.registerQuery("Y = load '"+BASIC_TABLE+"' using org.apache.hadoop.hive.howl.pig.HowlLoader() as (a:int);");
-    Schema dumpedYSchema = server.dumpSchema("Y");
+    server.registerQuery("Y1 = load '"+BASIC_TABLE+"' using org.apache.hadoop.hive.howl.pig.HowlLoader();");
+    server.registerQuery("Y2 = foreach Y1 generate a;");
+    Schema dumpedYSchema = server.dumpSchema("Y2");
 //    System.err.println("dumped schema Y : "+dumpedYSchema.toString());
     List<FieldSchema> Yfields = dumpedYSchema.getFields();
     assertEquals(1,Yfields.size());
@@ -257,7 +293,7 @@ public class TestHowlLoader extends TestCase {
     assertTrue(Yfields.get(0).type == DataType.INTEGER);
 
     int numTuplesRead = 0;
-    Iterator<Tuple> YIter = server.openIterator("Y");
+    Iterator<Tuple> YIter = server.openIterator("Y2");
     while( YIter.hasNext() ){
       Tuple t = YIter.next();
       assertEquals(t.size(),1);
@@ -265,6 +301,6 @@ public class TestHowlLoader extends TestCase {
       assertEquals(t.get(0),basicInputData.get(numTuplesRead).first);
       numTuplesRead++;
     }
-    assertEquals(numTuplesRead,basicInputData.size());
+    assertEquals(basicInputData.size(),numTuplesRead);
   }
 }
