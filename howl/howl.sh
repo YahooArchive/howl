97538a5395081ffd3351d4848d8f7d2c1b406aec
Ashutosh Chauhan <hashutosh@apache.org>
2010-10-27 18:12:40 -0700

    As always, there is something which you think off just after pushing. So, one more commit. Just has comments in howl.sh; nothing more.

--- howl/howl.sh
+++ howl/howl.sh
@@ -14,4 +14,5 @@ export HADOOP_OPTS=$HADOOP_OPTS
 
 exec $HADOOP_HOME/bin/hadoop jar  ${HOWL_JAR_LOC} org.apache.hadoop.hive.howl.cli.HowlCli "$@"
 
-
+# Above is the recommended way to launch howl cli. If it doesnt work, you can try the following:
+# java -Dhive.metastore.uris=thrift://localhost:9083 -cp ../lib/commons-logging-1.0.4.jar:../build/hadoopcore/hadoop-0.20.0/hadoop-0.20.0-core.jar:../lib/commons-cli-2.0-SNAPSHOT.jar:../build/cli/hive-cli-0.7.0.jar:../ql/lib/antlr-runtime-3.0.1.jar:$HOWL_JAR org.apache.hadoop.hive.howl.cli.HowlCli "$@"
