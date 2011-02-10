628b59bc0a76321dbbead0be3c15bf7450b03e75
Ashutosh Chauhan <hashutosh@apache.org>
2011-02-10 09:35:11 -0800

    Use CliDriver instead of Driver to take advantage of more parsing logic in it.

--- howl/src/java/org/apache/hadoop/hive/howl/oozie/JavaAction.java
+++ howl/src/java/org/apache/hadoop/hive/howl/oozie/JavaAction.java
@@ -1,12 +1,11 @@
 package org.apache.hadoop.hive.howl.oozie;
 
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hive.cli.CliDriver;
 import org.apache.hadoop.hive.cli.CliSessionState;
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.conf.HiveConf.ConfVars;
 import org.apache.hadoop.hive.howl.cli.SemanticAnalysis.HowlSemanticAnalyzer;
-import org.apache.hadoop.hive.ql.Driver;
-import org.apache.hadoop.hive.ql.processors.CommandProcessorResponse;
 import org.apache.hadoop.hive.ql.session.SessionState;
 
 public class JavaAction {
@@ -15,16 +14,10 @@ public class JavaAction {
 
     HiveConf conf = new HiveConf();
     conf.addResource(new Path("file:///", System.getProperty("oozie.action.conf.xml")));
-    conf.set(ConfVars.SEMANTIC_ANALYZER_HOOK.varname, HowlSemanticAnalyzer.class.getName());
+    conf.setVar(ConfVars.SEMANTIC_ANALYZER_HOOK, HowlSemanticAnalyzer.class.getName());
     conf.setBoolVar(ConfVars.METASTORE_USE_THRIFT_SASL, true);
     SessionState.start(new CliSessionState(conf));
-    Driver driver = new Driver(conf);
-    CommandProcessorResponse resp = driver.run(args[0]);
-    String errMsg = resp.getErrorMessage();
-    if(errMsg != null){
-      System.out.println("Error message while executing DDL command: "+errMsg);
-    }
-    System.out.println("Response code while executing DDL command: "+resp.getResponseCode());
+    new CliDriver().processLine(args[0]);
   }
 
 }
